27a6552ad181c41c04a6b7b390fe278b7c93200a
Manuvikash <manuvikashs@gmail.com>
2022-03-28 00:01:30 +0530

    started registeration fee table

--- src/components/Section8.js
+++ src/components/Section8.js
@@ -2,7 +2,7 @@ import Header from "./Header";
 const Section8 = () => {
   return (
     <section className="w-screen h-fit px-6 py-16 lg:px-[calc(100vw/12)] space-y-4 lg:flex lg:flex-col bg-green">
-      <div className="flex flex-col lg:flex-row items-start">
+      <div className=" lg:flex-row items-start">
         <Header theme="light" subtext="" text="registration" />
         <div className="flex-1 "></div>
         <div className="text-white lg:max-w-[60%] pt-16 font-poppins text-justify">
@@ -12,6 +12,55 @@ const Section8 = () => {
             accepted papers will be considered for presentation and publication
             only after the payment of registration fee.
           </p>
+        </div>
+        <div className="grid grid-rows-5 bg-white w-full h-fit mt-20 border-solid border-slate border-x-1">
+          <div className="bg-dark-green font-ubuntu flex items-center justify-center py-2 border-solid border-slate border text-white border-t-2">
+            Indian Delegates
+          </div>
+          <div className="bg-whitesmoke py-2 border-solid border-slate border font-poppins flex">
+            <p className="text-left mx-5">Academicians</p>
+            <p className=" bg-light-green relative right-2 rounded-lg px-5 py-0.5">
+              ₹ 2500
+            </p>
+          </div>
+          <div className="bg-whitesmoke py-2 border-solid border-slate border font-poppins flex">
+            <p className="text-left mx-5">
+              Industry / R&D Institutuion Participants
+            </p>
+            <p className=" bg-light-green relative right-2 rounded-lg px-5 py-0.5">
+              ₹ 1500
+            </p>
+          </div>
+          <div className="bg-whitesmoke py-2 border-solid border-slate border font-poppins flex">
+            <p className="text-left mx-5">Full Time Research Scholars</p>
+            <p className=" bg-light-green relative right-2 rounded-lg px-5 py-0.5">
+              ₹ 1000
+            </p>
+          </div>
+          <div className="bg-whitesmoke py-2 border-solid border-slate border-b-2 border font-poppins flex">
+            <p className="text-left mx-5">Accompanying guests / Spouse</p>
+            <p className="bg-light-green relative right-2 rounded-lg px-5 py-0.5">
+              ₹ 1000
+            </p>
+          </div>
+        </div>
+
+        <div className="grid grid-rows-3 bg-white w-full h-fit mt-20">
+          <div className="bg-dark-green font-ubuntu flex items-center justify-center py-2 border-solid border-slate border text-white border-t-2">
+            Foreign Delegates
+          </div>
+          <div className="bg-whitesmoke py-2 border-solid border-slate border font-poppins flex">
+            <p className="text-left mx-5">Academicians</p>
+            <p className="bg-light-green relative right-2 rounded-lg px-5 py-0.5">
+              200 USD
+            </p>
+          </div>
+          <div className="bg-whitesmoke py-2 border-solid border-slate border-b-2 border font-poppins flex">
+            <p className="text-left mx-5">Accompanying guests / Spouse</p>
+            <p className="bg-light-green relative right-2 rounded-lg px-5 py-0.5">
+              100 USD
+            </p>
+          </div>
         </div>
       </div>
       <div className=" items-start my-6 pt-8 lg:space-y-0">
